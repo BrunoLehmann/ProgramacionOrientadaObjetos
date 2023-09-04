1935060dbeff1308650256bc60a10e69195d3e39
BrunoLehmann <brunolehmann123@gmail.com>
2023-09-03 23:36:40 -0300

    Se agrega la clase Main con las respetivas pruebas.

---
+++ ProyectoTP1/src/Main.java
@@ -0,0 +1,70 @@
+import Objetos.ObjetosLista.Lista;
+import Objetos.ObjetosListaDobleEnlazada.ListaDobleEnlazada;
+import Objetos.ObjetosPila.Pila;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Trabajo Practico 1.");
+        /*
+     //PRUEBA DE LISTA SIMPLE ENLAZADA
+        Lista lista = new Lista();
+
+        lista.agregar("1");
+        lista.agregar("2");
+        lista.agregar("3");
+        lista.agregar("4");
+        System.out.println("Tamaño: " + lista.size());
+        System.out.println(lista);
+        lista.insertar(5, "F");
+
+        //lista.eliminarPos(0);
+        System.out.println(lista);
+        */
+
+        /*
+        //PRUEBA PARA LISTA ENLAZADA DOBLE
+        ListaDobleEnlazada listaDoble = new ListaDobleEnlazada();
+
+        listaDoble.agregar("1");
+        listaDoble.agregar("2");
+        listaDoble.agregar("3");
+        listaDoble.agregar("4");
+        listaDoble.agregar("5");
+        listaDoble.agregar("6");
+        System.out.println("Tamaño: " + listaDoble.longitud());
+        System.out.println(listaDoble);
+        listaDoble.insertar(5, "F");
+
+        listaDoble.eliminarPos(3);
+        System.out.println(listaDoble);
+        */
+
+        /*
+     // PRUEBA DE PILA
+        Pila pila = new Pila();
+        pila.apilar("1");
+        System.out.println("Tope: " + pila.getTope());
+        pila.apilar("2");
+        System.out.println("Tope: " + pila.getTope());
+        pila.apilar("3");
+        System.out.println("Tope: " + pila.getTope());
+        pila.apilar("4");
+        System.out.println("Tope: " + pila.getTope());
+        pila.apilar("5");
+        System.out.println("Tope: " + pila.getTope());
+
+        //Pruebo la funcion pila.toString();
+        System.out.println(pila);
+        System.out.println("Tope: " + pila.getTope());
+
+        System.out.println("Desapilado 1: " + pila.desapilar());
+        System.out.println("Desapilado 2: " +pila.desapilar());
+        System.out.println("Desapilado 3: " +pila.desapilar());
+        System.out.println("Desapilado 4: " +pila.desapilar());
+        System.out.println("Desapilado 5: " +pila.desapilar());
+        System.out.println("Desapilado con error: " + pila.desapilar());
+       */
+
+    }
+
+}
